8972e7acb14655767928519b34324836ebd827f1
Aryaman6492 <aryaman.g@seclogic.io>
2022-06-20 04:02:56 +0530

    Add Redshift cluster nodes extractors, add requirements.txt.

---
+++ versions/services/red::cluster.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from versions import register
+from versions.parser import MarkdownDoc, HtmlDoc
+import re
+
+
+@register
+def node_generation():
+	title = 'Redshift clusters latest generation of nodes'
+	url = 'https://docs.aws.amazon.com/redshift/latest/mgmt/working-with-clusters.html#working-with-clusters-overview'
+	doc = HtmlDoc(url)
+	node = {'current' : {}, 'previous': {}, 'recommended_upgrade' : {}}
+	table_headers = doc.find_all(id=re.compile(r'rs-(.+)-nodes-table'))
+
+	for header in table_headers:
+		table = header.find_next_sibling(class_="table-container")
+		headers = [header.text for header in table.find('thead').find_all('th')]
+		for row in table.find('tbody').find_all('tr'):
+			cells = row.find_all('td')
+			if headers[0] not in node['current']:
+				node['current'][headers[0]] = []
+			node['current'][headers[0]].append(cells[0].get_text(strip=True))
+
+
+	header = doc.find(id='rs-upgrading-to-ra3')
+	table = header.find_next_sibling(class_="table-container")
+	
+	headers = [header.text for header in table.find('thead').find_all('th')]
+	for row in table.find('tbody').find_all('tr'):
+		cells = row.find_all('td')
+		old = cells[0].get_text(strip=True)
+		new = cells[2].get_text(strip=True)
+		if old not in node['recommended_upgrade']:
+			node['recommended_upgrade'][old] = []
+		node['recommended_upgrade'][old].append({
+			headers[1] : cells[1].get_text(strip=True),
+			headers[2] : new,
+			headers[3] : cells[3].get_text(strip=True)
+		})
+
+	return {
+		'id' : __name__+'.node_generation',
+		'title' : title,
+		'node_generation' : node,
+	}

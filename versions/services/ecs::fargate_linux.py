f3cd8149e9cae752029e0d1d31e16017a3495ab8
Aryaman6492 <aryaman.g@seclogic.io>
2022-06-19 07:40:42 +0530

    Add Fargate extractors.

---
+++ versions/services/ecs::fargate_linux.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from versions import register
+from versions.parser import MarkdownDoc, HtmlDoc
+import re
+
+@register
+def platform_version():
+	title = 'Fargate Linux Platform Version'
+	url = 'https://docs.aws.amazon.com/AmazonECS/latest/userguide/platform-linux-fargate.html'
+	doc = HtmlDoc(url)
+	platform_versions = {'supported' : [], 'deprecated': []}
+	for version in doc.find_all(['h2', 'h1', 'h3'], id=re.compile(r'^platform-version-\d+-\d+')):
+		platform_versions['supported'].append(version.get_text(strip=True))
+	
+	url = 'https://docs.aws.amazon.com/AmazonECS/latest/developerguide/platform-versions-retired.html'
+	doc = HtmlDoc(url)
+	header = doc.find(id='platform-versions-retired')
+	table = header.parent.find(class_='table-contents')
+	headers = [header.text for header in table.find('thead').find_all('th')]
+	platform_versions['deprecated'] = [{headers[i]: cell.get_text(strip=True).strip() for i, cell in enumerate(row.find_all('td'))}
+		for row in table.find('tbody').find_all('tr')]
+
+	return {
+		'code' : __name__+'.platform_version',
+		'title' : title,
+		'platform_version' : platform_versions
+	}
---
+++ versions/services/ecs::fargate_windows.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from versions import register
+from versions.parser import MarkdownDoc, HtmlDoc
+import re
+
+@register
+def platform_version():
+	title = 'Fargate Windows Platform Version'
+	url = 'https://docs.aws.amazon.com/AmazonECS/latest/developerguide/platform-windows-fargate.html'
+	doc = HtmlDoc(url)
+	platform_versions = {'supported' : [], 'deprecated': []}
+	for version in doc.find_all(['h2', 'h1', 'h3'], id=re.compile(r'^platform-version-w\d+-\d+')):
+		platform_versions['supported'].append(version.get_text(strip=True))
+
+	return {
+		'code' : __name__+'.platform_version',
+		'title' : title,
+		'platform_version' : platform_versions
+	}

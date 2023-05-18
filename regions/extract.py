cb0a50c5bad3301f6b57b6ad11684ae7bc49a24e
Aryaman6492 <98874173+Aryaman6492@users.noreply.github.com>
2023-05-18 09:39:56 +0000

    Add comparsion for regions and update workflow.
    Aws regions extraction now supports comparision from old and new json providing added/removed regions or services.
    Updated workflow for the same.

--- regions/extract.py
+++ regions/extract.py
@@ -1,37 +1,99 @@
 #!/usr/bin/python3
+from datetime import datetime
+
+import os
 import json
 import requests
 import pandas as pd
 
-data = requests.get('https://api.regional-table.region-services.aws.a2z.com/index.json').json()
-js = {'index_by_region': {}, 'index_by_service' : {}}
+def main():
+	data = requests.get('https://api.regional-table.region-services.aws.a2z.com/index.json', timeout = 10).json()
+	js = {'index_by_region': {}, 'index_by_service' : {}}
+
+	cjs = load_cache('AwsServiceRegions.json', jsonstr = True)
+
+	for service in data['prices']:
+		region_name = service['attributes']['aws:region']
+		service_name = service['attributes']['aws:serviceName']
+
+		if not js['index_by_region'].get(region_name):
+			js['index_by_region'][region_name] = []
+
+		js['index_by_region'][region_name].append(service_name)
 
 
-for service in data['prices']:
-	region_name = service['attributes']['aws:region']
-	service_name = service['attributes']['aws:serviceName']
+		if not js['index_by_service'].get(service_name):
+			js['index_by_service'][service_name] = []
 
-	if not js['index_by_region'].get(region_name):
-		js['index_by_region'][region_name] = []
+		js['index_by_service'][service_name].append(region_name)
 
-	js['index_by_region'][region_name].append(service_name)
+	df = pd.DataFrame(index=js['index_by_service'].keys(), columns=js['index_by_region'].keys())
+
+	for service in data['prices']:
+		region_name = service['attributes']['aws:region']
+		service_name = service['attributes']['aws:serviceName']
+
+		df.at[service_name,region_name] = 'YES'
+
+	styler = df.style.applymap(lambda x: "background-color: #23C552" if x == 'YES' else "background-color: #F84F31")
+
+	# Cache our data in json and xlsx
+	save_cache('AwsServiceRegions.json', jsonstr = True, data = js)
+	save_cache('AwsServiceRegions.xlsx', jsonstr = False, data = styler)
+
+	# Compare new and old data and send changes notification
+	send_notification(compare(js, cjs))
+
+def compare(new, old):
+	"""Compares old and new json and checks for new or discarded regions and services."""
+	default = 'No changes were made.'
+	region_added = ', '.join( set(new['index_by_region'].keys()) - set(old['index_by_region'].keys()) )
+	region_removed = ', '.join( set(old['index_by_region'].keys()) - set(new['index_by_region'].keys()) )
+	service_added = ', '.join( set(new['index_by_service'].keys()) - set(old['index_by_service'].keys()) )
+	service_removed = ', '.join( set(old['index_by_service'].keys()) - set(new['index_by_service'].keys()) )
+
+	return (f'*New Regions :*\n{region_added or default}\n\n' 
+			f'*Discarded Regions :*\n{region_removed or default}\n\n' 
+			f'*New Services :*\n{service_added or default}\n\n' 
+			f'*Discarded Services :*\n{service_removed or default}\n\n'
+		)
 
 
-	if not js['index_by_service'].get(service_name):
-		js['index_by_service'][service_name] = []
+def load_cache(file_name, jsonstr = True):
+	"""Load the service regions data either from a json or xlxs."""
 
-	js['index_by_service'][service_name].append(region_name)
+	try:
+		with open(file_name) as f:
+			if jsonstr:
+				return json.load(f)
 
-df = pd.DataFrame(index=js['index_by_service'].keys(), columns=js['index_by_region'].keys())
+	except (FileNotFoundError, json.decoder.JSONDecodeError):
+		return {}
 
-for service in data['prices']:
-	region_name = service['attributes']['aws:region']
-	service_name = service['attributes']['aws:serviceName']
+def save_cache(file_name, jsonstr = True, data = None):
+	"""Save the service regions data either to a json or xlxs."""
+	if jsonstr:
+		with open(file_name, 'w+') as f:
+			f.write(json.dumps(data))
+	else:
+		data.to_excel(file_name)
 
-	df.at[service_name,region_name] = 'YES'
+def send_notification(message):
+    """Hangouts Chat incoming webhook quickstart."""
 
-styler = df.style.applymap(lambda x: "background-color: #23C552" if x == 'YES' else "background-color: #F84F31")
-print(df)
-styler.to_excel('Regional Services.xlsx')
+    url = os.environ['NOTIFICATION_URL']
+    bot_message = {'text': f'Notification {datetime.now()}\n\n{message}'}
+    message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
-#print(json.dumps(js))
+    response = requests.post(
+        url,
+        headers=message_headers,
+        json=bot_message,
+		timeout=10
+    )
+    return response
+
+
+if __name__ == '__main__':
+	main()
+	#print(json.dumps(js))
